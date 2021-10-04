907f3a5c6028c7a65d829ea7729b1c796ba83fac
ititiu19223 <ititiu19223@student.hcmiu.edu.vn>
2021-10-04 19:35:16 +0700

    Ex 1, 2, 3

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -1,14 +1,3 @@
-/*
-2.	Write a program to receive integers, the program ends when user input 0. 
-Print the minimum (different from 0) and maximum number (different from 0) among the received numbers.
-Ex:
- ____________________________________
-| Input: -3 5 -2 9 8 10 5 -1 0       |
-| Output: Max: 10                    |
-|         Min: -3                    |
-|____________________________________|
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -29,18 +18,15 @@ int main(int argc, char *argv[])
 			testcase[i] = atoi(argv[i + 1]);
 	}
 	//Your codes here
-	int max, min;
+	int max = 0, min = 1e6;
 	for (int i = 0; i < argc; i++)
 	{
 		if (max < testcase[i])
-		{
 			max = testcase[i];
-		}
 		if (min > testcase[i])
-		{
 			min = testcase[i];
-		}
 	}
+
 	printf("Max: %d", max);
 	printf("Min: %d", min);
 	return 0;
