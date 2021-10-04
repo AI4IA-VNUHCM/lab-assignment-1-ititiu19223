cdf5deddfd566d4e18a1f9c75a5a5e93ff90a552
ititiu19223 <ititiu19223@student.hcmiu.edu.vn>
2021-10-04 19:25:40 +0700

    Ex 1, 2, 3

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -13,19 +13,35 @@ Ex:
 #include <stdlib.h>
 #include <math.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	//testing variable, applying it to your algorithm for auto-evaluating
 	argc--;
-	int testcase[argc],i;
-	for(i=0; i<argc;i++){
-		if(atoi(argv[i+1])==0){
+	int testcase[argc], i;
+	for (i = 0; i < argc; i++)
+	{
+		if (atoi(argv[i + 1]) == 0)
+		{
 			argc = i;
 			break;
 		}
 		else
-			testcase[i] = atoi(argv[i+1]);
+			testcase[i] = atoi(argv[i + 1]);
 	}
 	//Your codes here
-
+	int max, min;
+	for (int i = 0; i < argc; i++)
+	{
+		if (max < testcase[i])
+		{
+			max = testcase[i];
+		}
+		if (min > testcase[i])
+		{
+			min = testcase[i];
+		}
+	}
+	printf("Max: %d", max);
+	printf("Min: %d", min);
 	return 0;
 }
